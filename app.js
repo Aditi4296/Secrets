a10d81ca0416f6a9e656cb7f96a7bc1e354aa94f
Aditi4296 <aditisingh4296@rediffmail.com>
2023-07-14 23:44:16 +0530

    made user account more secure using salting ans hashing

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption");
+// const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -23,7 +26,7 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
+// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User",userSchema);
 
@@ -40,33 +43,60 @@ app.get("/register",(req,res)=>{
 });
 
 app.post("/register",(req,res)=>{
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
-    })
-    newUser.save().then( () => {
-        res.render("secrets");
-      }).catch( (err) => {
-        console.log('There was an error', e.message);
-      });
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        // Store hash in your password DB.
+        const newUser = new User({
+            email: req.body.username,
+            // password: req.body.password
+            // password: md5(req.body.password)
+            password: hash
+        })
+        newUser.save().then( () => {
+            res.render("secrets");
+          }).catch( (err) => {
+            console.log('There was an error', e.message);
+          });
+    });
+    
 });
 
 app.post("/login",(req,res)=>{
+    
     const username = req.body.username;
     const password = req.body.password;
+    // const password = md5(req.body.password);
 
     User.findOne({email: username})
     .then(function(foundUser){
+        // if(foundUser){
+        //     if(foundUser.password === password){
+        //         res.render("secrets");
+        //     }
+        //     else{
+        //         console.log("Wrong password");
+        //     }
+        // }else{
+        //     console.log("User not found");
+        // }
         if(foundUser){
-            if(foundUser.password === password){
-                res.render("secrets");
-            }
-            else{
-                console.log("Wrong password");
-            }
-        }else{
-            console.log("User not found");
+            bcrypt.compare(password, foundUser.password).then(function(result) {
+                // result == true
+                if(result == true)
+                {
+                    res.render("secrets");
+                }
+            });
+            bcrypt.compare(password, foundUser.password).then(function(result) {
+                // result == false
+                if(result == false)
+                {
+                    console.log("Wrong password");
+                }
+            });
         }
+        
+        
     })
     .catch(function(err){
         console.log(err);
